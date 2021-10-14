820191a6e3ddd83ab6d7ed5cbafdf39c6206d3e9
Colin Sidberry <colinsidberry@gmail.com>
2021-10-14 13:45:24 -0700

    finished the handleSubmit function and the form returned by NewBoxForm component

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -18,18 +18,45 @@ function NewBoxForm({ createBoxes }) {
         setBackgroundColor(evt.target.value);
     }
 
-    //handleSubmit
-    
     function handleSubmit(evt) {
         evt.preventDefault();
-        //TODO: do something with the data submitted
+        createBoxes(height, width, backgroundColor);
         setHeight(5);
         setWidth(5);
         setBackgroundColor("blue");
         console.log("Check out state ->", formData);
     }
-    
-    //TODO:return createBoX(passing in data)
+
+    //Question: When is it a better design to have the 
+    // form element in the Parent component and just pass in the input elements
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="height">Height:</label>
+            <input
+                id="height"
+                name="height"
+                value={height}
+                onChange={handleHeightChange}
+            />
+
+            <label htmlFor="width">Width:</label>
+            <input
+                id="width"
+                name="width"
+                value={width}
+                onChange={handleWidthChange}
+            />
+
+            <label htmlFor="backgroundColor">BackgroundColor:</label>
+            <input
+                id="backgroundColor"
+                name="backgroundColor"
+                value={backgroundColor}
+                onChange={handleBackgroundColorChange}
+            />
+            <button>Add a new box!</button>
+        </form>
+    );
 }
 
 export default NewBoxForm;
